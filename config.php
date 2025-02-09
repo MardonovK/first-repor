2092dd411c3198efa4a6c38aca34c33d16d9464b
Muadddip <marmarkaka1231@gmail.com>
2025-02-09 17:46:34 +0500

    new commit

--- config.php
+++ config.php
@@ -13,6 +13,6 @@ $password = '';
 $conn = new mysqli($host, $user, $password, $dbName);
 
 if ($conn->connect_error) {
-    die("Ulanishda xato: " . $conn->connect_error);
+    die("Ulanishda xato: "  . $conn->connect_error);
 }
 ?>
